e72cd1e602f7c7e917f4850f94497d666155455f
SAID <davedtysen714@gmail.com>
2019-10-31 00:14:22 +0100

    Updates

---
+++ precache-manifest.9d95ed0d29834cb8e9d9f8c68f89ac74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "25a8165c860618e428c66c67e15b517e",
+    "url": "/my-app-react/index.html"
+  },
+  {
+    "revision": "ff73cece9170797c829d",
+    "url": "/my-app-react/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "954e6c2a6dfdfaf72e25",
+    "url": "/my-app-react/static/js/2.421833ac.chunk.js"
+  },
+  {
+    "revision": "ff73cece9170797c829d",
+    "url": "/my-app-react/static/js/main.dd0320ea.chunk.js"
+  },
+  {
+    "revision": "d151a4a3244de67f025e",
+    "url": "/my-app-react/static/js/runtime-main.4d2079bc.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/my-app-react/static/media/logo.25bf045c.svg"
+  }
+]);
